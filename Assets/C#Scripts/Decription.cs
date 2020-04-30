88058d9f7dc5fcdd15357821f4ecca22ba07ea22
Ankorkish <58787660+Ankorkish@users.noreply.github.com>
2020-04-30 12:43:21 +0300

    Descriptions

---
+++ Assets/C#Scripts/Decription.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Decription : MonoBehaviour
+{
+    public GameObject MyDscription;
+    bool Active = false;
+    private void Start()
+    {
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            Active = true;
+            MyDscription.SetActive(true);
+        }
+
+    }
+    void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            MyDscription.SetActive(false);
+            }
+    }
+
+}
