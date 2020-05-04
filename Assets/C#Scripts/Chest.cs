072535a6b12d6dd28f04cf96c1bca73724e5d44d
Ankorkish <58787660+Ankorkish@users.noreply.github.com>
2020-05-04 11:55:07 +0300

    Chest

---
+++ Assets/C#Scripts/Chest.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chest : MonoBehaviour
+{
+    bool Active = false;
+    bool used = false;
+    Animator chest;
+    public GameObject[] Materials;
+    public GameObject MySes;
+    void Start()
+    {
+        chest = GetComponent<Animator>();
+    }
+    private void Update()
+    {
+        if (Active)
+        {
+            if (Input.GetKeyDown(KeyCode.R) && !used)
+            {
+                print("Awsome");
+                chest.SetTrigger("Open");
+                chest.SetBool("opened", true);
+                used = true;
+                StartCoroutine(InstateMaterials());
+                Destroy(MySes);
+            }
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            Active = true;
+        }
+
+    }
+    void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            Active = false;
+        }
+    }
+    IEnumerator InstateMaterials()
+    {
+        yield return new WaitForSeconds(0.3f);
+        for (int i = 0; i < 12; i++)
+        {
+            Instantiate(Materials[Random.Range(0, 3)], new Vector3(transform.position.x, transform.position.y, -49), Quaternion.identity);
+            yield return new WaitForSeconds(0.1f);
+
+        }
+    }
+
+}
+
